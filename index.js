67640aba9b1fa249715ebf5670b7a40ee26c2006
YanaEgorova <yanaegorova69.3@gmail.com>
2019-11-13 18:55:59 +0200

    +

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class CountdownTimer {
+
+  constructor(id, date) {
+    this._date = date;
+    this._id = id;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2019'),
+});
